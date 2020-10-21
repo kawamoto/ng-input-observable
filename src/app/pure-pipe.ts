32a7f3a547e73cd01a41b153bea9eb45ee9c9283
Yuki <kawamoto62@gmail.com>
2020-10-21 00:42:37 +0000

    add logger to pure-pipe

--- src/app/pure-pipe.ts
+++ src/app/pure-pipe.ts
@@ -3,6 +3,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 @Pipe({ name: "purePipe", pure: true })
 export class PurePipe implements PipeTransform {
   transform(data: number) {
+    console.log("PurePipe: transform");
     return data * 2;
   }
 }
